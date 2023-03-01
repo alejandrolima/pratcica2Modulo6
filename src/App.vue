a9325290b249a880be9571f85ce7c0bfb863a4d7
Ernesto Miranda <mirandainsanedevil@gmail.com>
2023-03-01 01:33:32 -0400

    cambio de menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
 
-  <nav>
-    <router-link to="/">Home</router-link> 
-    <router-link to="/about"> | About</router-link>
-    <router-link to="/dron"> | Drones</router-link>
+  <nav class="navbar navbar-expand-lg " v-bind:style="`background-color: ${configuracionPagina.menuColor}`">
+    <div class="container-fluid">
+        <a class="navbar-brand" >{{ configuracionPagina.marca }}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" @click="$router.push('/')">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" @click="$router.push('/about')"> | About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" @click="$router.push('/dron')"> | Drones</a>
+                </li>
+            </ul>
+        </div>
+    </div>
   </nav>
 
   <div class="container" style="max-width: 800px; margin: auto">
@@ -20,6 +38,17 @@ export default {
   name: 'AppView',
   components: {
     FooterView
+  },
+  data() {
+    return {
+      configuracionPagina:
+      {
+          marca: "MegaDron",
+          menuColor: "lightblue",
+          footerColor: "slategrey"         
+      }
+    }
+
   }
 }
 </script>
