998079844fc87d2433150e22dfcbd0d1515d56a1
Alejandro Lima Maldonado <allima69@gmail.com>
2023-04-06 01:50:03 -0400

    Adicion segun Participante

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Alejandro Lucio Lima Maldonado</h1>
+    <h1>Deybes Richard Condori Condori</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
